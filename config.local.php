b794968827145666786f2a00a6ed224394f93b9a
methaihuy <methaihuy1505@gmail.com>
2025-12-10 03:23:40 +0700

    Remove sensitive config file

--- config.local.php
+++
@@ -1,5 +0,0 @@
-<?php
-define("SERVERNAME", "localhost");
-define("USERNAME", "root");
-define("PASSWORD", "");
-define("DBNAME", "friend_connect");
--- config.prod.php
+++
@@ -1,5 +0,0 @@
-<?php
-define("SERVERNAME", "sql313.infinityfree.com");
-define("USERNAME", "if0_40609708");
-define("PASSWORD", "androissKZ12345");
-define("DBNAME", "if0_40609708_friend_connect");
