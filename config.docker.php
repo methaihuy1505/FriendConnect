de2edcb0843ea3bd369275dfb12ba9c3428a8af2
methaihuy <methaihuy1505@gmail.com>
2025-12-17 06:21:13 +0700

    update database credentials in Docker configuration

--- config.docker.php
+++ config.docker.php
@@ -1,6 +1,6 @@
 <?php
 
-define("SERVERNAME", "db");
-define("USERNAME", "root");
-define("PASSWORD", "");
+define("SERVERNAME", "db");    // tên service MySQL trong docker-compose
+define("USERNAME", "fc_user"); // user bình thường
+define("PASSWORD", "fc_pass"); // mật khẩu user bình thường
 define("DBNAME", "friend_connect");
